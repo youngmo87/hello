f9fe95af8805d4504ecd5ab3f21d8c4a4a399f09
youngmo87 <youngmo.jeju@gmail.com>
2019-04-19 11:32:17 +0900

    exam_revised

--- deep/0417-02.py
+++ deep/0417-02.py
@@ -17,15 +17,15 @@ score = metrics.accuracy_score(testLabel, pred)
 
 
 params = [
-    {"C": [1, 10, 100, 1000], "kernel": ['linear']},
-    {"C": [1, 10, 100, 1000], "kernel": ['rbf'], "gamma": [0.001, 0.0001]},
+    {"C": [1, 10, 100], "kernel": ['linear']},
+    {"C": [1, 10, 100], "kernel": ['rbf'], "gamma": [0.001, 0.0001]},
 ]
 
 clf = GridSearchCV(svm.SVC(), params, n_jobs = -1, cv = 3, iid=True)
 
 # clf = svm.SVC(gamma='auto')   # old training
-clf.fit(train['images'], train['images'])
+clf.fit(trainData, trainLabel)
 
 
-print("machine=", clf.best_estimator_)
+print("최적화값은:", clf.best_estimator_)
 print("Score=", score)
